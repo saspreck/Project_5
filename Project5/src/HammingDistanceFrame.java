28c659da52e0503dc3316745b4541e9a7d905da7
Skyler Sprecker <saspreck@gmail.com>
2019-04-30 09:03:23 -0500

    Worked on UI

--- Project5/src/HammingDistanceFrame.java
+++ Project5/src/HammingDistanceFrame.java
@@ -1,17 +1,19 @@
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
+import java.awt.Dimension;
+import java.awt.Insets;
+import javax.swing.JTable;
 /**
  * Class to create a GUI for calculating Hamming Distance
  * @author skylersprecker
@@ -26,23 +28,41 @@ public class HammingDistanceFrame extends JFrame implements MouseListener, Chang
 	 * Interactive panel that allows the user to 
 	 */
 	public HammingDistanceFrame() {
-		//calls super constructors and sets size, visibility, and close operation
+		//calls super constructors and sets size, visibility, close operation, and the layout manager
 		super("Hamming Distance Frame");
 		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new GridLayout(1,2));
 
+		//creates panel for the left side of the display
+		JPanel leftSide = new JPanel(new GridBagLayout());
+		leftSide.setPreferredSize(new Dimension(FRAME_WIDTH - 400, FRAME_HEIGHT));
 		
+		GridBagConstraints leftSideConst = null;
+		
+		
+		//label for box to set hamming distance
 		JLabel hammDist = new JLabel();
-		hammDist.setText("Enter Hamming Distance");
-		hammDist.setBounds(100, 100, 200, 20);
+		hammDist.setText("Enter Hamming Distance:");
 		hammDist.setVisible(true);
+		leftSideConst = new GridBagConstraints();
+		leftSideConst.gridx = 0;
+		leftSideConst.gridy = 0;
+		//leftSideConst.insets = new Insets(50, 50, 50, 50);
+		leftSide.add(hammDist, leftSideConst);
 		
+		//text field for hamming distance
 		JTextField hammDistInfo = new JTextField();
 		hammDistInfo.setEditable(false);
 		hammDistInfo.setText("2");
-		hammDistInfo.setBounds(325, 100, 50, 20);
 		hammDistInfo.setVisible(true);
+		leftSideConst = new GridBagConstraints();
+		leftSideConst.gridx = 1;
+		leftSideConst.gridy = 0;
+		//leftSideConst.insets = new Insets(50, 50, 50, 50);
+		leftSide.add(hammDistInfo, leftSideConst);
 		
+		//slider to select the hamming distance to compare
 		JSlider compDist = new JSlider(0, 4);
 		compDist.addChangeListener(this);
 		compDist.setMajorTickSpacing(1);
@@ -50,11 +70,41 @@ public class HammingDistanceFrame extends JFrame implements MouseListener, Chang
 		compDist.setPaintLabels(true);
 		compDist.setSnapToTicks(true);
 		compDist.setVisible(true);
+		leftSideConst = new GridBagConstraints();
+		leftSideConst.gridx = 0;
+		leftSideConst.gridy = 2;
+		//leftSideConst.insets = new Insets(50, 50, 50, 50);
+		leftSide.add(compDist, leftSideConst);
 		
-		this.add(hammDist);
-		this.add(hammDistInfo);
-		this.add(compDist);
+		JButton showStation = new JButton("Show Station");
+		showStation.setSize(new Dimension(20, 50));
+		leftSideConst = new GridBagConstraints();
+		leftSideConst.gridx = 0;
+		leftSideConst.gridy = 3;
+		//leftSideConst.insets = new Insets(50, 50, 50, 50);
+		leftSide.add(showStation, leftSideConst);
 		
+		JTable matchingStations = new JTable();
+		matchingStations.setVisible(true);
+		leftSideConst = new GridBagConstraints();
+		leftSideConst.gridx = 0;
+		leftSideConst.gridy = 4;
+		//leftSideConst.insets = new Insets(50, 50, 50, 50);
+		leftSide.add(matchingStations, leftSideConst);
+		
+		JLabel compareWith = new JLabel("Compare with:");
+		compareWith.setVisible(true);
+		leftSideConst = new GridBagConstraints();
+		leftSideConst.gridx = 0;
+		leftSideConst.gridy = 5;
+		leftSideConst.insets = new Insets(10, 10, 10, 10);
+		leftSide.add(compareWith, leftSideConst);
+		
+		
+		//adds panel to frame
+		this.add(leftSide);
+		
+		//makes everything visible
 		this.setVisible(true);
 		
 		
